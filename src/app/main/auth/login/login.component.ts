9fc178ac61aa17a121b5a990ecb480eb9b994db2
NShanmukh <nshanmuka9@gmail.com>
2021-02-28 09:37:44 +0530

    latest login setup

--- src/app/main/auth/login/login.component.ts
+++ src/app/main/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this._formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      remember: [''],
+      vendEmp: ['', Validators.required]
+    });
   }
 
 }
